82b0cbd56ef189d2f17ff86f7270d71deb0435db
Marcus Doran <marcus.doran@gmail.com>
2013-07-11 14:23:20 -0400

    Finish 7.6 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
 	subject { page }
 
 	describe "signup page" do
@@ -8,13 +8,26 @@ describe "UserPages" do
 
 		it { should have_content('Sign up') }
   		it { should have_title(full_title('Sign up')) }
+  	end
 
+  	describe "signup" do
+  		before { visit signup_path }
 		let(:submit) { "Create my account" }
 
 		describe "with invalid information" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+				before { click_button submit }
+					
+				it { should have_title('Sign up') }
+				it { should have_content('error') }
+			end
+
+			describe ""
+
 		end
 
 		describe "with valid information" do
@@ -27,6 +40,14 @@ describe "UserPages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
+
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
